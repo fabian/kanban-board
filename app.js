24770f6681a0b7b8c3cacf1117c0340cfe964ddf
Fabian Vogler <fabian@equivalence.ch>
2014-03-10 20:08:02 +0100

    Added Express middleware

--- app.js
+++ app.js
@@ -2,9 +2,15 @@
 
 var express = require('express');
 
-var app = express();
+var app = express(),
+    path = require('path');
 
 app.set('port', process.env.PORT || 3000);
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
 
 // load routes
 require('./routes')(app);
