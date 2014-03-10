d696a1f656f2027fe1c3e5f35da400f5262d0a54
Fabian Vogler <fabian@equivalence.ch>
2014-03-10 19:56:12 +0100

    Added Card model

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,8 +1,10 @@
 "use strict";
 
+var Card = require('../models/Card');
+
 exports.list = function(req, res){
 
-    var cards = [];
+    var cards = Card.list();
 
     res.send({
         cards: cards
---
+++ models/Card.js
@@ -0,0 +1,11 @@
+
+var Card = function () {
+
+};
+
+Card.list = function () {
+    return [];
+};
+
+module.exports = Card;
+
